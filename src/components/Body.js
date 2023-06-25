7fa3691bc8cec9c7b9a7f09e5fb8803c3118e9cc
Harrshaa <sriharsha.mutakodoor@gmail.com>
2023-06-26 00:47:42 +0530

    useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useState,useEffect} from "react";
 import resList from "../utils/mockData";
 import RestaurentCard from "./Restaurentcard";
 
 const Body =()=>{
     //Local State Variable - Super Powerful Variable//
-
     let [listOfRestaurents,setlistOfRestaurents]=useState(resList);
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData = async()=>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.5366218&lng=78.4844811&page_type=DESKTOP_WEB_LISTING"
+        );
+
+        const json =await data.json();
+        console.log(json);
+    
+    
+    };
 
     return(
          <div className='body'>
