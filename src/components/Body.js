80dd070bfbe8d476b680b19e2d2977318dd619fa
Harrshaa <sriharsha.mutakodoor@gmail.com>
2023-07-04 12:13:12 +0530

    search bar implemented using useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,20 +10,25 @@ import { useState } from "react";
 
 const Body =()=>{
     //Local State Variable - Super Powerful Variable//
-    const [listOfRestaurents,setlistOfRestaurents]=useState(resList);
-    
+    const [listOfRestaurents,setlistOfRestaurents]=useState([]);
+    const [filteredRestaurents,setfilteredRestaurents]=useState([]);
+    const [searchText,setsearchText]=useState("");   
+     
 
     useEffect(()=>{
         fetchData();
     },[]);
+
 
     const fetchData = async()=>{
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.5366218&lng=78.4844811&page_type=DESKTOP_WEB_LISTING");
         const json =await data.json();
         console.log(json);
         //Optional Chaining//
-        setlistOfRestaurents(json.data.cards[0].data.data.cards); 
-    };  
+        setlistOfRestaurents(json.data.cards[2].data.data.cards);
+        setfilteredRestaurents(json.data.cards[2].data.data.cards);
+    };
+
 
     return(
          <div className='body'>
@@ -31,12 +36,19 @@ const Body =()=>{
             <div className='filter'>
 
             <div className="search">
-                <input type="text" className="search-box" placeholder="search it"></input>
-                <button onClick={()=>{
+                <input type="text" className="search-box" placeholder="search it" value={searchText} onChange={(e)=>{setsearchText(e.target.value)}}/>
+                <button className="searchButton" onClick={()=>{
                     //Filter the Restaurent cards and update UI//
-                }}></button>
+                    const filtered=listOfRestaurents.filter(
+                        (res)=>res.data.name.toLowerCase().includes(searchText)
+                        );
+                        setfilteredRestaurents(filtered);                  
+                    
+                }}>Search</button>
             </div>
-                <button className="filter-btn" 
+
+            
+            <button className="filter-btn" 
                 onClick={()=>{
                     const filteredList=listOfRestaurents.filter(
                         (res)=> res.data.avgRating >4                       
@@ -45,13 +57,17 @@ const Body =()=>{
                     }}>Top Rated Restaurents</button>
 
             </div>
-            <div className='res-container'>               
-                
-                {listOfRestaurents.map((restaurent)=>(
-                    <RestaurentCard key={restaurent.data.id} resData ={restaurent} />
+
+
+
+            <div className='res-container'>              
+                {filteredRestaurents.map((restaurent)=>(
+                <RestaurentCard key={restaurent.data.id} resData ={restaurent} />
                 ))}              
-                
             </div>
+
+
+
          </div>
     );
 }
