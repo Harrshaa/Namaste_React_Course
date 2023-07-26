2acf5811da513d0ce656bbec613202e1d5735f94
Harrshaa <sriharsha.mutakodoor@gmail.com>
2023-07-26 18:56:04 +0530

    Class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
+import UserClass from "./UserClass";
+import User from "./user";
+
 const About =()=>{
     return(
         <div>
             <h1>About</h1>
             <h2>This is Namaste React Web Series</h2>
+            <User name={"Harsha (functional)"} location={"Hyderabad"} contact={"7780791364"} />
+            <UserClass name={"Harsha (class)"} location={"Hyderabad"} contact={"7780791364"}/>
+            
         </div>
     )
 }
-
 export default About;
